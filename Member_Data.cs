155b2af24fa8386fc939ff5a96e8c8eb46f32353
Vinnie Venuti <46759252+torrone2019@users.noreply.github.com>
2019-03-03 18:52:57 -0800

    Add files via upload
    C# Course Methods and exceptions Class files

--- Member_Data.cs
+++ Member_Data.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Drawing;
+using SSS_Exceptions;
 
 public class Member_Data
 {
 
     /// <summary>
-    /// 
+    /// Enums
     /// </summary>
 
     enum EyeColorType
@@ -57,9 +58,12 @@ public class Member_Data
     }
 
     private static int lastMemnberNumber = 0;
+
+    private static int SeattleZipCode = 98052;
+
     #region Properties
     /// <summary>
-    /// AcccountNummber
+    /// MembershipNummber
     /// </summary>
     public int MembershipNumber { get; }
     /*
@@ -70,7 +74,7 @@ public class Member_Data
     public float Weight { get; set; }
     public string ScreenName { get; set; }
     public HairColorType HairColor { get; set; }
-    public EyeColorType  EyeColor { get; set; }
+    public EyeColorType EyeColor { get; set; }
     public DateTime CreateDate { get; }
     /*
      * DOB: date of birth: Current time - DOB = Age
@@ -84,4 +88,185 @@ public class Member_Data
 
     #endregion
 
+    #region Methods
+    /*
+    * Methods
+    */
+
+    var _usZipRegEx = @"^\d{5}(?:[-\s]\d{4})?$";
+   
+    private bool IsUSZipCode(string zipCode)
+    {
+        var validZipCode = false;
+        if (Regex.Match(zipCode, _usZipRegEx).Success)
+        {
+            validZipCode = true;
+        }
+        return validZipCode;
+    }
+
+private bool ValidiateZipcode (int Zipcode)
+    {
+        if (IsUSZipCode)
+        {
+            // This needs to be fix in the next version of the code. Use a method alreay written.
+            //Console.WriteLine(" Invalid Zip Code, the Zip Code must be in the Seattle Washington State Area");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine(" Invalid Zip Code, the Zip Code is not a valid US Zip Code");
+            return false;
+        }
+    }
+
+private bool ValidiateHeight(int HeightFeetPart,  int HeightInchPart)
+    {
+        try
+        {
+            Height = CalculateHeight(HeightPartFeet, HeightPartInch);
+            return true;
+        }
+        catch (Exception e)
+        {
+            // Process all other exceptions
+            Console.WriteLine(e.Message);
+            return false;
+        }
+        catch (ZeroHeightException e)
+        {
+            return false;
+        }
+        catch (InvalidHeightException e)
+        {
+            return false;
+        }
+    }
+
+    public int CalculateHeight(int HeightFeetPart, int HeightInchPart)
+    {
+        TotalHeightInInches = (HeightFeetPart * 12) + HeightInchPart;
+        if (Height == 0)
+        {
+            Console.WriteLine(" Invalid Height, Height must be between 4 ft 0 inches and 7 ft 0 inches");
+            throw new ZeroHeightException(Height);
+        }
+        else if (48 < Height > 84)
+        {
+            throw new InvalidHeightException(Height);
+        }
+        else
+        {
+            return TotalHeightInInches;
+        }
+    }
+
+    private bool ValidiateWeight(int Weight)
+    {
+        if (Weight == 0)
+        {
+            throw new ZeroWeightException(Weight);
+        }
+        else if (100 < Weight > 300)
+        {
+            throw new InvalidWeightException(Weight);
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public static string GetAge(DateTime dob)
+    {
+        DateTime todayDateUtc = DateTime.UtcNow;
+        DateTime pastYearDate;
+        int years = 0;
+        string age;
+        int days;
+
+        if (DateTime.UtcNow > dob)
+        {
+            years = new DateTime(DateTime.UtcNow.Subtract(dob).Ticks).Year - 1;
+        }
+
+        pastYearDate = dob.AddYears(years);
+        days = todayDateUtc.Subtract(pastYearDate).Days;
+        age = years + " years " + days + " days";
+
+        return age;
+    }
+
+    private bool ValidiateDOB(DateTime DOB)
+    {
+        DateTime deltaTime = 0;
+        DateTime currentTime = DateTime.Now;
+
+        GetAge(DOB);
+
+        if (21 < age > 60)
+        {
+            Console.WriteLine(" Invalid Age, the age must be between 21 and 60");
+            return false;
+        }
+    }
+
+    public bool ValidiateEmail(string emailaddress)
+    {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+    }
+
+    private bool ValidiateScreenName(string ScreenName)
+    {
+        /*
+         *    Check if ScreenName is Null or Empty
+         */
+
+        if (IsNullOrEmpty(ScreenName))
+        {
+            throw new ScreenNameNullException(ScreenName);
+        }
+        else if (CheckIfScreenNameAvailable(ScreenName))
+        {
+            return true;
+        }
+        else
+        {
+            throw new ScreenNameInUseException(ScreenName);
+        }
+    }
+
+    public bool CheckIfScreenNameAvailable(string ScName)
+    /*
+     *    Loop thru all entries in the ScreenNames array
+     */
+    {
+        foreach (int i in ScreenNameArray)
+        {
+            if (ScName = ScreenNameArray[i])
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public bool UploadPictures(Image Picture, int indx, int PictureIndx)
+    {
+        /*
+         *    Put the pictures in the Member's account
+         */
+        Members[indx].Picture[PictureIndx] = Pictures;
+    }
+
+    #endregion
 }
---
+++ SSS_Exceptions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSS_Exceptions
+{
+    public class ZeroWeightException : ApplicationException
+    {
+        public ZeroWeightException() { }
+        public ZeroWeightException(int value) : base(String.Format("Warning: Weight is Zero: {0}", value)) { }
+        public ZeroWeightException(string message,
+                                   System.Exception inner) : base(message, inner) { }
+    }
+
+    public class InvalidWeightException : ApplicationException
+    {
+        public InvalidWeightException() { }
+        public InvalidWeightException(int value) : base(String.Format("Exception: Weight {0} is not bewtween 100 and 300 lbs.", value)) { }
+        public InvalidWeightException(string message,
+                                   System.Exception inner) : base(message, inner) { }
+    }
+
+    public class ZeroHeightException : ApplicationException
+    {
+        public ZeroHeightException() { }
+        public ZeroHeightException(int value) : base(String.Format("Warning: Height is Zero: {0}", value)) { }
+        public ZeroHeightException(string message,
+                                   System.Exception inner) : base(message, inner) { }
+    }
+
+    public class InvalidHeightException : ApplicationException
+    {
+        public InvalidHeightException() { }
+        public InvalidHeightException(int value) : base(String.Format("Exception: Height {0} is not between 4 and  7 ft", value)) { }
+        public InvalidHeightException(string message,
+                                   System.Exception inner) : base(message, inner) { }
+    }
+
+    public class ScreenNameNullException : ApplicationException
+    {
+        public ScreenNameNullException() { }
+        public ScreenNameNullException(string entry) : base(String.Format("Exception: Sceenname {0} cannot be NULL", entry)) { }
+        public ScreenNameNullException(string message,
+                                   System.Exception inner) : base(message, inner) { }
+    }
+
+    public class ScreenNameInUseException : ApplicationException
+    {
+        public ScreenNameInUseException() { }
+        public ScreenNameInUseException(string entry) : base(String.Format("Excdption Screenname {0} is already in use", entry)) { }
+        public ScreenNameInUseException(string message,
+                                   System.Exception inner) : base(message, inner) { }
+    }
+
+    public class OtherException : ApplicationException
+    {
+        public OtherException() { }
+        public OtherException(string message) : base(message) { }
+        public OtherException(string message,
+                                   System.Exception inner) : base(message, inner) { }
+    }
+
+}
